a3cc7d40a9570216316e34e4a81aa89f319f79e3
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2023-10-06 16:10:26 -0700

    adds loading and resets state when the user selects a new age range

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Stack from 'react-bootstrap/Stack';
+import Spinner from 'react-bootstrap/Spinner';
 import AgeForm from '../components/AgeForm';
 import SearchForm from '../components/SearchForm';
 import Translation from '../components/Translation';
@@ -8,15 +9,36 @@ const Main = () => {
 
   const [userAgeGroup, setUserAgeGroup] = useState('');
   const [translationResult, setTranslationResult] = useState('');
+  const [loading, setLoading] = useState(false);
 
   let handleUserAgeSelect = (event) => {
+    setTranslationResult('');
+    setLoading(false);
     setUserAgeGroup(event.target.value);
   }
 
   return (
     <Stack gap={5}>
       <AgeForm handleUserAgeSelect={handleUserAgeSelect} />
-      { userAgeGroup && <SearchForm />}
+      {
+        userAgeGroup && (
+          <>
+            <SearchForm
+              userAgeGroup={userAgeGroup}
+              setTranslationResult={setTranslationResult}
+              setLoading={setLoading}
+            />
+            {
+              loading && <Spinner animation="border" variant="primary" />
+            }
+
+            { 
+              translationResult && <Translation />
+            }
+          </>
+        )
+      }
+
     </Stack>
   )
 }
